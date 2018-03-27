83e6d20a0b0d132d6a5969a90b815feb185932f6
小倩 <13240016260@163.com>
2018-03-27 10:11:54 +0800

    Update run.py

--- src/run.py
+++ src/run.py
@@ -50,57 +50,55 @@ Next, it triggers the simulator to run for the specified number of episodes.
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--dict_path', dest='dict_path', type=str, default='./deep_dialog/data/dicts.v3.p', help='path to the .json dictionary file')
-    parser.add_argument('--movie_kb_path', dest='movie_kb_path', type=str, default='./deep_dialog/data/movie_kb.1k.p', help='path to the movie kb .json file')
-    parser.add_argument('--act_set', dest='act_set', type=str, default='./deep_dialog/data/dia_acts.txt', help='path to dia act set; none for loading from labeled file')
-    parser.add_argument('--slot_set', dest='slot_set', type=str, default='./deep_dialog/data/slot_set.txt', help='path to slot set; none for loading from labeled file')
-    parser.add_argument('--goal_file_path', dest='goal_file_path', type=str, default='./deep_dialog/data/user_goals_first_turn_template.part.movie.v1.p', help='a list of user goals')
-    parser.add_argument('--diaact_nl_pairs', dest='diaact_nl_pairs', type=str, default='./deep_dialog/data/dia_act_nl_pairs.v6.json', help='path to the pre-defined dia_act&NL pairs')
-
-    parser.add_argument('--max_turn', dest='max_turn', default=20, type=int, help='maximum length of each dialog (default=20, 0=no maximum length)')
-    parser.add_argument('--episodes', dest='episodes', default=1, type=int, help='Total number of episodes to run (default=1)')
-    parser.add_argument('--slot_err_prob', dest='slot_err_prob', default=0.05, type=float, help='the slot err probability')
-    parser.add_argument('--slot_err_mode', dest='slot_err_mode', default=0, type=int, help='slot_err_mode: 0 for slot_val only; 1 for three errs')
-    parser.add_argument('--intent_err_prob', dest='intent_err_prob', default=0.05, type=float, help='the intent err probability')
-    
-    parser.add_argument('--agt', dest='agt', default=0, type=int, help='Select an agent: 0 for a command line input, 1-6 for rule based agents')
-    parser.add_argument('--usr', dest='usr', default=0, type=int, help='Select a user simulator. 0 is a Frozen user simulator.')
-    
-    parser.add_argument('--epsilon', dest='epsilon', type=float, default=0, help='Epsilon to determine stochasticity of epsilon-greedy agent policies')
-    
-    # load NLG & NLU model
-    parser.add_argument('--nlg_model_path', dest='nlg_model_path', type=str, default='./deep_dialog/models/nlg/lstm_tanh_relu_[1468202263.38]_2_0.610.p', help='path to model file')
-    parser.add_argument('--nlu_model_path', dest='nlu_model_path', type=str, default='./deep_dialog/models/nlu/lstm_[1468447442.91]_39_80_0.921.p', help='path to the NLU model file')
-    
-    parser.add_argument('--act_level', dest='act_level', type=int, default=0, help='0 for dia_act level; 1 for NL level')
-    parser.add_argument('--run_mode', dest='run_mode', type=int, default=0, help='run_mode: 0 for default NL; 1 for dia_act; 2 for both')
-    parser.add_argument('--auto_suggest', dest='auto_suggest', type=int, default=0, help='0 for no auto_suggest; 1 for auto_suggest')
-    parser.add_argument('--cmd_input_mode', dest='cmd_input_mode', type=int, default=0, help='run_mode: 0 for NL; 1 for dia_act')
-    
-    # RL agent parameters
+ parser = argparse.ArgumentParser()   
+    #often change params
+    # agt==9(DQN)
+    parser.add_argument('--agt', dest='agt', default=9, type=int, help='Select an agent: 0 for a command line input, 1-6 for rule based agents')
+    #usr==1（使用模拟器）
+    parser.add_argument('--usr', dest='usr', default=1, type=int, help='Select a user simulator. 0 is a Frozen user simulator.')
+    #run_mode 0 (NL)运行模式; 1(Dia_Act)debug模式; 2(Dia_Act and NL)debug模式; 3(training或者predict)非运行模式
+    parser.add_argument('--run_mode', dest='run_mode', type=int, default=3, help='run_mode: 0 for default NL; 1 for dia_act; 2 for both')   
+    #act_level Dia_Act&NL
+    parser.add_argument('--act_level', dest='act_level', type=int, default=1, help='0 for dia_act level; 1 for NL level')
+     #（DQN隐藏层层数）
+    parser.add_argument('--dqn_hidden_size', dest='dqn_hidden_size', type=int, default=80, help='the hidden size for DQN')
+    #others
+    parser.add_argument('--max_turn', dest='max_turn', default=40, type=int, help='maximum length of each dialog (default=20, 0=no maximum length)')
+    parser.add_argument('--episodes', dest='episodes', default=500, type=int, help='Total number of episodes to run (default=1)')
+    parser.add_argument('--epsilon', dest='epsilon', type=float, default=0, help='Epsilon to determine stochasticity of epsilon-greedy agent policies')     
+    parser.add_argument('--warm_start', dest='warm_start', type=int, default=1, help='0: no warm start; 1: warm start for training')
+    parser.add_argument('--warm_start_epochs', dest='warm_start_epochs', type=int, default=120, help='the number of epochs for warm start')
     parser.add_argument('--experience_replay_pool_size', dest='experience_replay_pool_size', type=int, default=1000, help='the size for experience replay')
-    parser.add_argument('--dqn_hidden_size', dest='dqn_hidden_size', type=int, default=60, help='the hidden size for DQN')
+    parser.add_argument('--simulation_epoch_size', dest='simulation_epoch_size', type=int, default=100, help='the size of validation set') 
+    parser.add_argument('-o', '--write_model_dir', dest='write_model_dir', type=str, default='D:\\MyProject\\MiuLab-TC-Bot-master\\MiuLab-TC-Bot-master\\src\\deep_dialog\\checkpoints\\rl_agent\\', help='write model to disk') 
+    parser.add_argument('--slot_err_prob', dest='slot_err_prob', default=0.00, type=float, help='the slot err probability')
+    parser.add_argument('--slot_err_mode', dest='slot_err_mode', default=0, type=int, help='slot_err_mode: 0 for slot_val only; 1 for three errs')
+    parser.add_argument('--intent_err_prob', dest='intent_err_prob', default=0.00, type=float, help='the intent err probability')  
     parser.add_argument('--batch_size', dest='batch_size', type=int, default=16, help='batch size')
+    parser.add_argument('--goal_file_path', dest='goal_file_path', type=str, default='D:\\MyProject\\MiuLab-TC-Bot-master\\MiuLab-TC-Bot-master\\src\\deep_dialog\\data\\user_goals_first_turn_template.part.movie.v1.p', help='a list of user goals') 
+    #seldom change params
+    parser.add_argument('--dict_path', dest='dict_path', type=str, default='D:\\MyProject\\MiuLab-TC-Bot-master\\MiuLab-TC-Bot-master\\src\\deep_dialog\\data\\dicts.v3.p', help='path to the .json dictionary file')
+    parser.add_argument('--movie_kb_path', dest='movie_kb_path', type=str, default='D:\\MyProject\\MiuLab-TC-Bot-master\\MiuLab-TC-Bot-master\\src\\deep_dialog\\data\\movie_kb.1k.p', help='path to the movie kb .json file')
+    parser.add_argument('--act_set', dest='act_set', type=str, default='D:\\MyProject\\MiuLab-TC-Bot-master\\MiuLab-TC-Bot-master\\src\\deep_dialog\\data\\dia_acts.txt', help='path to dia act set; none for loading from labeled file')
+    parser.add_argument('--slot_set', dest='slot_set', type=str, default='D:\\MyProject\\MiuLab-TC-Bot-master\\MiuLab-TC-Bot-master\\src\\deep_dialog\\data\\slot_set.txt', help='path to slot set; none for loading from labeled file')
+    parser.add_argument('--diaact_nl_pairs', dest='diaact_nl_pairs', type=str, default='D:\\MyProject\\MiuLab-TC-Bot-master\\MiuLab-TC-Bot-master\\src\\deep_dialog\\data\\dia_act_nl_pairs.v6.json', help='path to the pre-defined dia_act&NL pairs')
+    # load NLG & NLU model
+    parser.add_argument('--nlg_model_path', dest='nlg_model_path', type=str, default='D:\\MyProject\\MiuLab-TC-Bot-master\\MiuLab-TC-Bot-master\\src\\deep_dialog\\models\\nlg\\lstm_tanh_relu_[1468202263.38]_2_0.610.p', help='path to model file')
+    parser.add_argument('--nlu_model_path', dest='nlu_model_path', type=str, default='D:\\MyProject\\MiuLab-TC-Bot-master\\MiuLab-TC-Bot-master\\src\\deep_dialog\\models\\nlu\\lstm_[1468447442.91]_39_80_0.921.p', help='path to the NLU model file')       
+    parser.add_argument('--auto_suggest', dest='auto_suggest', type=int, default=0, help='0 for no auto_suggest; 1 for auto_suggest')
+    parser.add_argument('--cmd_input_mode', dest='cmd_input_mode', type=int, default=0, help='run_mode: 0 for NL; 1 for dia_act')   
+    # RL agent parameters
     parser.add_argument('--gamma', dest='gamma', type=float, default=0.9, help='gamma for DQN')
     parser.add_argument('--predict_mode', dest='predict_mode', type=bool, default=False, help='predict model for DQN')
-    parser.add_argument('--simulation_epoch_size', dest='simulation_epoch_size', type=int, default=50, help='the size of validation set')
-    parser.add_argument('--warm_start', dest='warm_start', type=int, default=1, help='0: no warm start; 1: warm start for training')
-    parser.add_argument('--warm_start_epochs', dest='warm_start_epochs', type=int, default=100, help='the number of epochs for warm start')
-    
     parser.add_argument('--trained_model_path', dest='trained_model_path', type=str, default=None, help='the path for trained model')
-    parser.add_argument('-o', '--write_model_dir', dest='write_model_dir', type=str, default='./deep_dialog/checkpoints/', help='write model to disk') 
-    parser.add_argument('--save_check_point', dest='save_check_point', type=int, default=10, help='number of epochs for saving model')
-     
-    parser.add_argument('--success_rate_threshold', dest='success_rate_threshold', type=float, default=0.3, help='the threshold for success rate')
-    
+    parser.add_argument('--save_check_point', dest='save_check_point', type=int, default=10, help='number of epochs for saving model')    
+    parser.add_argument('--success_rate_threshold', dest='success_rate_threshold', type=float, default=0.3, help='the threshold for success rate')   
     parser.add_argument('--split_fold', dest='split_fold', default=5, type=int, help='the number of folders to split the user goal')
-    parser.add_argument('--learning_phase', dest='learning_phase', default='all', type=str, help='train/test/all; default is all')
-    
+    parser.add_argument('--learning_phase', dest='learning_phase', default='all', type=str, help='train\\test\\all; default is all')
+    #parse
     args = parser.parse_args()
     params = vars(args)
-
+    #print
     print 'Dialog Parameters: '
     print json.dumps(params, indent=2)
 
